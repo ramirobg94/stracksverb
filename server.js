968eb375028ee7312bbdff9cfe3d48556e035e38
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2016-01-19 10:42:27 +0100

    limpieza

--- server.js
+++ server.js
@@ -9,64 +9,63 @@ var app = express();
 var storage = multer.diskStorage({
   destination: function (req, file, callback) {
 //console.log(file)
-    callback(null, '/mnt/nas');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.originalname);
-  }
+callback(null, '/mnt/nas');
+},
+filename: function (req, file, callback) {
+  callback(null, file.originalname);
+}
 });
 var storageC = multer.diskStorage({
   destination: function (req, file, callback) {
 //console.log(file)
-    callback(null, '/mnt/nas/covers');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.originalname);
-  }
+callback(null, '/mnt/nas/covers');
+},
+filename: function (req, file, callback) {
+  callback(null, file.originalname);
+}
 });
-var upload = multer({ storage : storage}).single('userPhoto');
+var upload = multer({ storage : storage}).single('track');
 var uploadCover = multer({ storage : storageC}).single('coverPhoto');
 
 app.use(methodOverride('_method'));
 
 app.get('/',function(req,res){
-      res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.use(express.static('public'));
-app.use('/public', express.static('public'));
 
 app.get('/media/:nombreTrack',function(req,res){
-      var nombre = req.params.nombreTrack;
-	console.log('/mnt/nas/'+nombre);
-	res.sendFile('/mnt/nas/'+nombre);
+  var nombre = req.params.nombreTrack;
+  console.log('/mnt/nas/'+nombre);
+  res.sendFile('/mnt/nas/'+nombre);
 });
 app.get('/media/covers/:nombreTrack',function(req,res){
-      var nombre = req.params.nombreTrack;
-	console.log('cover : /mnt/nas/covers/'+nombre);
-	res.sendFile('/mnt/nas/covers/'+nombre);
+  var nombre = req.params.nombreTrack;
+  console.log('cover : /mnt/nas/covers/'+nombre);
+  res.sendFile('/mnt/nas/covers/'+nombre);
 });
 
 
-app.post('/api/photo',function(req,res){
+app.post('/api/track',function(req,res){
   console.log("alguienLlama");
   var request = req;
 
-   if (request.method == 'POST') {
-          var body = '';
+  if (request.method == 'POST') {
+    var body = '';
   //console.log(request.data);
 
 
-          request.on('data', function (data) {
-              body += data;
-          });
+  request.on('data', function (data) {
+    body += data;
+  });
 
-          request.on('end', function () {
-  console.log(body);
-  console.log(body.fieldname);
-  console.log("fielName0");
-              var post = qs.parse(body);
-  console.log(post.file);
+  request.on('end', function () {
+    console.log(body);
+    console.log(body.fieldname);
+    console.log("fielName0");
+    var post = qs.parse(body);
+    console.log(post.file);
   // var postData = req.body;
           //var writeStream = fs.createWriteStream(fileName);
       /*    if(postData)
@@ -74,70 +73,60 @@ app.post('/api/photo',function(req,res){
           else{   
               console.log("failed miserably");
               console.log(postData);
-          }*/
+            }*/
           });
-      }
+}
   //console.log(req);
   //console.log("alguienLlama2");
-      upload(req,res,function(err) {
+  upload(req,res,function(err) {
   //console.log(req);
-          if(err) {
-              return res.end("Error uploading file.");
-          }
-          res.end("File is uploaded");
-      });
+  if(err) {
+    return res.end("Error uploading track.");
+  }
+  res.end("track is uploaded");
+});
 });
 
 app.post('/api/photoCover',function(req,res){
   console.log("alguienLlamaCover");
   var request = req;
-   if (request.method == 'POST') {
-          var body = '';
-          request.on('data', function (data) {
-              body += data;
-          });
-          request.on('end', function () {
-  console.log(body);
-  console.log(body.fieldname);
-  console.log("fielName0");
-              var post = qs.parse(body);
-  console.log(post.file);
-          });
-      }
-      uploadCover(req,res,function(err) {
-          if(err) {
-              return res.end("Error uploading file.");
-          }
-          res.end("File is uploaded");
-      });
-});
-
-
-app.post('/api/photoRm',function(req,res){
-    fs.unlink('./public/digital3a.png',function (err){
-  if (err) throw err;
-  console.log('successfully deleted /tmp/hello');
-res.end("File is deleted");
-
-});
+  if (request.method == 'POST') {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      console.log(body);
+      console.log(body.fieldname);
+      console.log("fielName0");
+      var post = qs.parse(body);
+      console.log(post.file);
+    });
+  }
+  uploadCover(req,res,function(err) {
+    if(err) {
+      return res.end("Error uploading cover.");
+    }
+    res.end("cover is uploaded");
+  });
 });
 
 app.delete('/delete/:trackName',function(req,res){
 	console.log("quiere que borre");
 	console.log(req.params.trackName);
-name = req.params.trackName;
+  name = req.params.trackName;
 
-fs.unlink('/mnt/nas/'+ name,function (err){
-  if (err) throw err;
-  console.log('successfully deleted /tmp/hello');
-res.end("File is deleted");
+  fs.unlink('/mnt/nas/'+ name,function (err){
+    if (err) throw err;
+    fs.unlink('/mnt/nas/covers/'+ name,function (err){
+      if (err) throw err;
+      console.log('successfully deleted track and cover');
+      res.end("File is deleted");
+    });
+  });
 
 });
-
-});
-
-
 
 app.listen(3000,function(){
-    console.log("Working on port 3000");
+  console.log("Working on port 3000");
 });
